752a4e2ad54a40a5b8eddd402c8fd57a3a32eecf
Aidan Shafran <zelbrium@gmail.com>
2017-02-10 18:28:57 -0500

    Cleaned up demo, window resizing supported

--- demo.cpp
+++ demo.cpp
@@ -1,30 +1,24 @@
 /*
-*2 * Aidan Shafran <zelbrium@gmail.com>, 2016.
+ * Aidan Shafran <zelbrium@gmail.com>, 2017.
  * 
  * Demo code for GLLabel. Depends on GLFW3, GLEW, GLM, FreeType2, and C++11.
- * Makefile was created for use on Arch Linux. I haven't tested it elsewhere.
+ * Currently requires Noto Sans and Droid Sans fonts installed.
+ * Only tested on Arch Linux.
  */
 
 #include "label.hpp"
 #include <glfw3.h>
 #include <glm/gtx/transform.hpp>
-#include <locale>
 #include <codecvt>
-#include <sstream>
-#include <iostream>
 #include <iomanip>
-#include <fstream>
 
-static const uint32_t kWidth = 16*70*2;//1536;
-static const uint32_t kHeight = 10*70*2;//1152;
+static uint32_t width = 1280;
+static uint32_t height = 800;
 
 static GLLabel *Label;
 static bool spin = false;
 static FT_Face defaultFace;
 static FT_Face boldFace;
-static FT_Face cjkFace;
-static bool fuzz = true;
-static bool regular = true;
 float horizontalTransform = 0.0;
 float verticalTransform = 0.8;
 float scale = 1;
@@ -32,93 +26,9 @@ float scale = 1;
 void onKeyPress(GLFWwindow *window, int key, int scanCode, int action, int mods);
 void onCharTyped(GLFWwindow *window, unsigned int codePoint, int mods);
 void onScroll(GLFWwindow *window, double deltaX, double deltaY);
+void onResize(GLFWwindow *window, int width, int height);
 std::u32string toUTF32(const std::string &s);
-static GLuint loadShaderProgramFile(const char *vertexShaderPath, const char *fragmentShaderPath);
-
-struct CPoint
-{
-	glm::vec3 coordinate;
-	glm::vec3 color;
-	glm::vec2 uv;
-};
-
-static glm::vec3 pt(float pt)
-{
-	static const float dpiScale = 96.0 / 72.0;
-	static const float emUnits = 1.0/2048.0;
-	static const float aspect = (float)kHeight / (float)kWidth;
-
-	float scale = emUnits * pt / 72.0;// * dpiScale / (kHeight/2.0);
-	return glm::vec3(scale * aspect, scale, 0);
-}
-
-static void setTextFile(std::string path)
-{
-	std::ifstream file(path);
-	std::string str;
-	std::string file_contents;
-	while (std::getline(file, str))
-	{
-		file_contents += str;
-		file_contents.push_back('\n');
-	}
-	
-	Label->SetText(toUTF32(file_contents), 1, glm::vec4(0,0,0,1), defaultFace);
-	Label->SetCaretPosition(Label->GetText().size());
-}
-
-static void loadNextText()
-{
-	verticalTransform = 0.8;
-	horizontalTransform = 0;
-	static int i = 0;
-	scale = 1;
-	switch(i)
-	{
-	default:
-		i=0;
-	case 0:
-		Label->SetText(U"Welcome to vector-based GPU text rendering!\nType whatver you want!\n\nPress LEFT/RIGHT to move cursor.\nPress ESC to toggle rotate.\nPress TAB to change files.\nPress UP to enable warps.\nPress DOWN to switch warps.\nPress SHIFT + UP/DOWN to move.\nHold ALT to type in RAINBOW!!!!\n\n", 1, glm::vec4(0.5,0,0,1), defaultFace);
-
-		Label->AppendText(U"t", 1, glm::vec4(1,0,0,1), defaultFace);
-		Label->AppendText(U"y", 1, glm::vec4(0,1,0,1), defaultFace);
-		Label->AppendText(U"p", 1, glm::vec4(0,0,1,1), defaultFace);
-		Label->AppendText(U"e", 1, glm::vec4(0,1,1,1), defaultFace);
-		Label->AppendText(U"!", 1, glm::vec4(1,1,0,1), defaultFace);
-		Label->SetCaretPosition(Label->GetText().size());
-		break;
-	
-//	case 1:
-//		setTextFile("q1.txt");
-//		break;
-//	case 2:
-//		setTextFile("q2.txt");
-//		break;
-//	case 3:
-//		setTextFile("q3.txt");
-//		break;
-//	case 4:
-//		setTextFile("q4.txt");
-//		break;
-	case 1:
-		//scale = 1.1;
-		horizontalTransform = 0.7;
-		verticalTransform = -0;
-		scale = 4;
-		Label->SetText(U"このテキストは日本語です.", 1, glm::vec4(0,0,0,1), cjkFace);
-		break;
-	case 2:
-		scale = 0.7;
-		verticalTransform = 5;
-		setTextFile("label.cpp");
-		Label->SetCaretPosition(0);
-		break;
-	case 3:
-		Label->SetText(U"Rendering Vector-Based\n      Text on the GPU\n\n         Aidan Shafran", 1, glm::vec4(0.1, 0.3, 0.8, 1), boldFace);
-	}
-	
-	i++;
-}
+static glm::vec3 pt(float pt);
 
 int main()
 {
@@ -132,13 +42,13 @@ int main()
 	glfwWindowHint(GLFW_SAMPLES, 8);
 	glfwWindowHint(GLFW_DEPTH_BITS, 0);
 	glfwWindowHint(GLFW_STENCIL_BITS, 0);
-	// glfwWindowHint(GLFW_ALPHA_BITS, 8);
+	glfwWindowHint(GLFW_ALPHA_BITS, 8);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	
-	GLFWwindow *window = glfwCreateWindow(kWidth, kHeight, "Vector-Based GPU Text Rendering", NULL, NULL);
+	GLFWwindow *window = glfwCreateWindow(width, height, "Vector-Based GPU Text Rendering", NULL, NULL);
 	if(!window)
 	{
 		fprintf(stderr, "Failed to create GLFW window.");
@@ -149,6 +59,7 @@ int main()
 	glfwSetKeyCallback(window, onKeyPress);
 	glfwSetCharModsCallback(window, onCharTyped);
 	glfwSetScrollCallback(window, onScroll);
+	glfwSetWindowSizeCallback(window, onResize);
 	
 	// Create OpenGL context
 	glfwMakeContextCurrent(window);
@@ -170,105 +81,26 @@ int main()
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_BLEND);
 	
+	// Create new label
 	Label = new GLLabel();
 	Label->ShowCaret(true);
+
+	printf("Loading font files. If this crashes, you probably don't have Droid Sans and Noto Sans installed at /usr/share/fonts.\n");
 	defaultFace = GLFontManager::GetFontManager()->GetDefaultFont();
 	boldFace = GLFontManager::GetFontManager()->GetFontFromPath("/usr/share/fonts/TTF/DroidSans-Bold.ttf");
-	cjkFace = GLFontManager::GetFontManager()->GetFontFromPath("/usr/share/fonts/TTF/DroidSansJapanese.ttf");
-	loadNextText();
 	
-	glm::mat4 kT0 = glm::translate(glm::mat4(), glm::vec3(0, 0, 0));
+	Label->SetText(U"Welcome to vector-based GPU text rendering!\nType whatver you want!\n\nPress LEFT/RIGHT to move cursor.\nPress ESC to toggle rotate.\nScroll vertically/horizontally to move.\nScroll while holding shift to zoom.\nRight-shift for bold.\nHold ALT to type in ", 1, glm::vec4(0.5,0,0,1), defaultFace);
+	Label->AppendText(U"r", 1, glm::vec4(0.58, 0, 0.83, 1), defaultFace);
+	Label->AppendText(U"a", 1, glm::vec4(0.29, 0, 0.51, 1), defaultFace);
+	Label->AppendText(U"i", 1, glm::vec4(0,    0, 1,    1), defaultFace);
+	Label->AppendText(U"n", 1, glm::vec4(0,    1, 0,    1), defaultFace);
+	Label->AppendText(U"b", 1, glm::vec4(1,    1, 0,    1), defaultFace);
+	Label->AppendText(U"o", 1, glm::vec4(1,    0.5, 0,  1), defaultFace);
+	Label->AppendText(U"w", 1, glm::vec4(1,    0, 0,    1), defaultFace);
+	Label->AppendText(U"!\n", 1, glm::vec4(0.5,0,0,1), defaultFace);
+	Label->SetCaretPosition(Label->GetText().size());
 	
 	GLLabel fpsLabel;
-	glm::mat4 fpsTransform = glm::scale(glm::translate(glm::mat4(), glm::vec3(-1, 0.86, 0)), pt(10));
-	
-	GLuint fb=0;
-	glGenFramebuffers(1, &fb);
-	glBindFramebuffer(GL_FRAMEBUFFER, fb);
-	// GLenum fboBuffers[] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
-	// glDrawBuffers(2, fboBuffers);
-	// glFramebufferTexture2D(CP_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, mainBuffer, 0);
-	
-	GLuint fbTex;
-	glGenTextures(1, &fbTex);
-	glBindTexture(GL_TEXTURE_2D, fbTex);
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, kWidth, kHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-	
-	// The depth buffer
-	GLuint depthrenderbuffer;
-	glGenRenderbuffers(1, &depthrenderbuffer);
-	glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, kWidth, kHeight);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
-	
-	// Set "renderedTexture" as our colour attachement #0
-	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, fbTex, 0);
-
-	// Set the list of draw buffers.
-	GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
-	glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
-	
-	// Always check that our framebuffer is ok
-	if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-		printf("ERRORRRR\n");
-	
-	// float fullscreenQuad[] = // x, y, z, u, v
-	// {
-	// 	-0.5, -0.5, 0, 0, 0,
-	// 	0.5, -0.5, 0, 1, 0,
-	// 	-0.5, 0.5, 0, 0, 1,
-	// 	
-	// 	0.5, -0.5, 0, 1, 0,
-	// 	0.5, 0.5, 0, 1, 1,
-	// 	-0.5, 0.5, 0, 0, 1
-	// };
-	
-	float size = 1;
-	
-	std::vector<CPoint> points;
-	CPoint a = {glm::vec3(size, size, 0), glm::vec3(1,1,0), glm::vec2(1, 1)};
-	CPoint b = {glm::vec3(size, -size, 0), glm::vec3(1,0,1), glm::vec2(1, 0)};
-	CPoint c = {glm::vec3(-size, -size, 0), glm::vec3(1,1,1), glm::vec2(0, 0)};
-	CPoint d = {glm::vec3(-size, size, 0), glm::vec3(0,1,1), glm::vec2(0, 1)};
-	points.push_back(a);	
-	points.push_back(b);
-	points.push_back(c);
-	points.push_back(a);
-	points.push_back(c);
-	points.push_back(d);
-	
-	// // 
-	// static const float fullscreenQuad[] = {
-	//    -1.0f, -1.0f, 0.0f,
-	//    1.0f, -1.0f, 0.0f,
-	//    0.0f,  1.0f, 0.0f,
-	// };
-	// 
-	GLuint fsQuadBuffer;
-	glGenBuffers(1, &fsQuadBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, fsQuadBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(CPoint)*points.size(), &points[0], GL_STATIC_DRAW);
-	// printf("size: %i\n", sizeof(fullscreenQuad));
-	
-	GLuint fuzzShader = loadShaderProgramFile("vert.glsl", "frag.glsl");
-	GLuint warpShader = loadShaderProgramFile("vert.glsl", "frag2.glsl");
-	glUseProgram(warpShader);
-	// printf("warp: %i\n", warpShader);
-	
-	GLuint uWarpTime = glGetUniformLocation(warpShader, "t");
-	GLuint uWarpSampler = glGetUniformLocation(warpShader, "uSampler");
-	glUniform1i(uWarpSampler, 0);
-	
-	glUseProgram(fuzzShader);
-
-	GLuint uFuzzTime = glGetUniformLocation(fuzzShader, "t");
-	GLuint uFuzzSampler = glGetUniformLocation(fuzzShader, "uSampler");
-	glUniform1i(uFuzzSampler, 0);
-
 	
 	int fpsFrame = 0;
 	double fpsStartTime = glfwGetTime();
@@ -276,63 +108,27 @@ int main()
 	{
 		float time = glfwGetTime();
 		
-		// glActiveTexture(GL_TEXTURE0);
-		glBindFramebuffer(GL_FRAMEBUFFER, regular ? 0 : fb);
 		glClearColor(160/255.0, 169/255.0, 175/255.0, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
-		//
-		glm::mat4 S0 = glm::scale(glm::mat4(), pt(8));
-		glm::mat4 T = glm::translate(glm::mat4(), glm::vec3(-0.9 + horizontalTransform, verticalTransform, 0));
-		glm::mat4 R = glm::rotate(glm::mat4(), time/3, glm::vec3(0.0,0.0,1.0));
-		glm::mat4 S = glm::scale(glm::mat4(), glm::vec3(sin(time)/6000.0, cos(time)/12000.0, 1.0));
-		glm::mat4 S1 = glm::scale(glm::mat4(), glm::vec3(scale, scale, 1.0));
-		Label->Render(time, spin ? (kT0*R*S) : (S1*T*S0));
-		// 
-		// // glViewport(0,0,kWidth,kHeight);
-		// // glClearColor(1, 0, 0, 0.1);
-		// // glClear(GL_COLOR_BUFFER_BIT);
-		// // glBindBuffer(GL_ARRAY_BUFFER, fsQuadBuffer);
-		// // glEnableVertexAttribArray(0);
-		// // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(CPoint), (void*)0);
-		// // glVertexAttribPointer(1, 3, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(float), (void*)(sizeof(float)*3));
-		// // glVertexAttribPointer(2, 2, GL_FLOAT, GL_TRUE, sizeof(float), (void*)(sizeof(float)*6));
-		// // glUseProgram(colorShader);
-		// // glDrawArrays(GL_TRIANGLES, 0, 6);
-		// // glDisableVertexAttribArray(0);
-		// 
-		if(!regular)
+
+		if(spin)
 		{
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		glClearColor(160/255.0, 169/255.0, 175/255.0, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT);
-	
-		glViewport(0,0,kWidth,kHeight);
-		// 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, fbTex);
-		glBindBuffer(GL_ARRAY_BUFFER, fsQuadBuffer);
-		glEnableVertexAttribArray(0);
-		glEnableVertexAttribArray(1);
-		glEnableVertexAttribArray(2);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(CPoint), (void*)0);
-		glVertexAttribPointer(1, 3, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(CPoint), (void*)(sizeof(float)*3));
-		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(CPoint), (void*)(sizeof(float)*6));
-		if(fuzz)
-		{
-			glUseProgram(fuzzShader);
-			glUniform2f(uFuzzTime, time*100, time*100);
+			glm::mat4 mat = glm::scale(glm::mat4(),
+				glm::vec3(sin(time)/6000.0, cos(time)/12000.0, 1.0));
+			mat = glm::rotate(mat, time/3, glm::vec3(0.0,0.0,1.0));
+			Label->Render(time, mat);
 		}
 		else
 		{
-			glUseProgram(warpShader);
-			glUniform2f(uWarpTime, time, time);
+			glm::mat4 kS = glm::scale(glm::mat4(), pt(8));
+			glm::mat4 mat = glm::scale(glm::mat4(), glm::vec3(scale, scale, 1.0));
+			mat = glm::translate(mat,
+				glm::vec3(-0.9 + horizontalTransform, verticalTransform, 0));
+			Label->Render(time, mat*kS);
 		}
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
-		glDisableVertexAttribArray(2);
-	}
 		
+		// Window size might change, so recalculate this (and other pt() calls)
+		glm::mat4 fpsTransform = glm::scale(glm::translate(glm::mat4(), glm::vec3(-1, 0.86, 0)), pt(10));
 		fpsLabel.Render(time, fpsTransform);
 		
 		glfwPollEvents();
@@ -395,49 +191,13 @@ void onKeyPress(GLFWwindow *window, int key, int scanCode, int action, int mods)
 	{
 		spin = !spin;
 	}
-	else if(key == GLFW_KEY_LEFT && (mods & GLFW_MOD_SHIFT) != GLFW_MOD_SHIFT)
+	else if(key == GLFW_KEY_LEFT)
 	{
 		Label->SetCaretPosition(Label->GetCaretPosition() - 1);
 	}
-	else if(key == GLFW_KEY_RIGHT && (mods & GLFW_MOD_SHIFT) != GLFW_MOD_SHIFT)
-	{
-		Label->SetCaretPosition(Label->GetCaretPosition() + 1);
-	}
-	else if(key == GLFW_KEY_DOWN && (mods & GLFW_MOD_SHIFT) != GLFW_MOD_SHIFT)
-	{
-		fuzz = !fuzz;
-	}
-	else if(key == GLFW_KEY_UP && (mods & GLFW_MOD_SHIFT) != GLFW_MOD_SHIFT)
-	{
-		regular = !regular;
-	}
-	else if(key == GLFW_KEY_DOWN)
-	{
-		verticalTransform += 0.2 / scale;
-	}
-	else if(key == GLFW_KEY_UP)
-	{
-		verticalTransform -= 0.2 / scale;
-	}
-	else if(key == GLFW_KEY_LEFT)
-	{
-		horizontalTransform += 0.2 / scale;
-	}
 	else if(key == GLFW_KEY_RIGHT)
 	{
-		horizontalTransform -= 0.2 / scale;
-	}
-	//else if(key == GLFW_KEY_F3)
-	//{
-	//	defaultFace = GLFontManager::GetFontManager()->GetFontFromPath("/usr/share/fonts/TTF/DroidSans-Bold.ttf");
-	//}
-	//else if(key == GLFW_KEY_F4)
-	//{
-	//	defaultFace = GLFontManager::GetFontManager()->GetDefaultFont();
-	//}
-	else if(key == GLFW_KEY_TAB)
-	{
-		loadNextText();
+		Label->SetCaretPosition(Label->GetCaretPosition() + 1);
 	}
 }
 
@@ -471,80 +231,26 @@ void onScroll(GLFWwindow *window, double deltaX, double deltaY)
 	}
 }
 
+void onResize(GLFWwindow *window, int w, int h)
+{
+	width = w;
+	height = h;
+	glViewport(0,0,w,h);
+}
+
 std::u32string toUTF32(const std::string &s)
 {
 	std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
 	return conv.from_bytes(s);
 }
 
-static GLuint loadShaderProgramFile(const char *vertexShaderPath, const char *fragmentShaderPath)
+// Converts font points into a glm::vec3 scalar.
+static glm::vec3 pt(float pt)
 {
-	// Compile vertex shader
-	std::ifstream vsStream(vertexShaderPath);
-	std::string vsCode((std::istreambuf_iterator<char>(vsStream)), (std::istreambuf_iterator<char>()));
-	const char *vsCodeC = vsCode.c_str();
-	
-	GLuint vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShaderId, 1, &vsCodeC, NULL);
-	glCompileShader(vertexShaderId);
-	
-	GLint result = GL_FALSE;
-	int infoLogLength = 0;
-	glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(vertexShaderId, GL_INFO_LOG_LENGTH, &infoLogLength);
-	if(infoLogLength > 1)
-	{
-		std::vector<char> infoLog(infoLogLength+1);
-		glGetShaderInfoLog(vertexShaderId, infoLogLength, NULL, &infoLog[0]);
-		printf("[Vertex] %s\n", &infoLog[0]);
-	}
-	if(!result)
-		return 0;
+	static const float dpiScale = 96.0 / 72.0;
+	static const float emUnits = 1.0/2048.0;
+	const float aspect = (float)height / (float)width;
 
-	// Compile fragment shader
-	std::ifstream fsStream(fragmentShaderPath);
-	std::string fsCode((std::istreambuf_iterator<char>(fsStream)), (std::istreambuf_iterator<char>()));
-	const char *fsCodeC = fsCode.c_str();
-	
-	GLuint fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShaderId, 1, &fsCodeC, NULL);
-	glCompileShader(fragmentShaderId);
-	
-	result = GL_FALSE, infoLogLength = 0;
-	glGetShaderiv(fragmentShaderId, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(fragmentShaderId, GL_INFO_LOG_LENGTH, &infoLogLength);
-	if(infoLogLength > 1)
-	{
-		std::vector<char> infoLog(infoLogLength);
-		glGetShaderInfoLog(fragmentShaderId, infoLogLength, NULL, &infoLog[0]);
-		printf("[Fragment] %s\n", &infoLog[0]);
-	}
-	if(!result)
-		return 0;
-	
-	// Link the program
-	GLuint programId = glCreateProgram();
-	glAttachShader(programId, vertexShaderId);
-	glAttachShader(programId, fragmentShaderId);
-	glLinkProgram(programId);
-
-	result = GL_FALSE, infoLogLength = 0;
-	glGetProgramiv(programId, GL_LINK_STATUS, &result);
-	glGetProgramiv(programId, GL_INFO_LOG_LENGTH, &infoLogLength);
-	if(infoLogLength > 1)
-	{
-		std::vector<char> infoLog(infoLogLength+1);
-		glGetProgramInfoLog(programId, infoLogLength, NULL, &infoLog[0]);
-		printf("[Shader Linker] %s\n", &infoLog[0]);
-	}
-	if(!result)
-		return 0;
-
-	glDetachShader(programId, vertexShaderId);
-	glDetachShader(programId, fragmentShaderId);
-	
-	glDeleteShader(vertexShaderId);
-	glDeleteShader(fragmentShaderId);
-
-	return programId;
+	float scale = emUnits * pt / 72.0;
+	return glm::vec3(scale * aspect, scale, 0);
 }
